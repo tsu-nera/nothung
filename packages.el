0909336fa2dc9c92ce7cf52708309e3c2a25a476
tsu-nera <fox10225fox@gmail.com>
2022-09-15 12:02:05 +0900

    :sparkles:add bm.el

--- packages.el
+++ packages.el
@@ -71,6 +71,8 @@
 (package! svg-tag-mode)
 (package! org-preview-html)
 
+(package! bm)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
